70fd088fdb36fb64bfa9dc6176812a5afed9c11f
VanV <vainavinair2009558@gmail.com>
2023-04-27 08:02:13 +0530

    added messages

--- users/views.py
+++ users/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate
+from django.contrib import messages
+
 from .forms import UserRegisterForm
+
 
 def register(request):
     if request.method == 'POST':  
         form = UserRegisterForm(request.POST)  
         if form.is_valid():  
+            username=form.cleaned_data.get('username')
             form.save()
+            messages.success(request,f"Account created for {username}!")
             return redirect('user-login')
     else:
         form = UserRegisterForm()
