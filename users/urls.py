143c0a7814c585287e61eaf3d7b2e42b119ac726
VanV <vainavinair2009558@gmail.com>
2023-04-27 14:35:25 +0530

    added email verificaiton views

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register
+from .views import register, token_send, success
 from django.contrib.auth import views as auth_views
 
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('register/',register, name='user-register'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='user-login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='user-logout'),
+    path('token/',token_send, name='user-token'),
+    path('success/',success, name='user-success'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
+import uuid
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.contrib import messages
-
+from user_profile.models import Profile
 from .forms import UserRegisterForm
 
 
@@ -10,9 +12,24 @@ def register(request):
         form = UserRegisterForm(request.POST)  
         if form.is_valid():  
             username=form.cleaned_data.get('username')
-            form.save()
-            messages.success(request,f"Account created for {username}!")
-            return redirect('user-login')
+            user = form.save()
+            profile_obj = Profile.objects.create(user=user, auth_token=str(uuid.uuid4()))
+            profile_obj.save()
+            # messages.success(request,f"Account created for {username}!")
+            return redirect('user-token')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html',{'form': form}) 
+
+def success(request):
+    return render(request,'users/success.html')
+
+def token_send(request):
+    return render(request,'users/token.html')
+
+def send_mail(email,token):
+    subject = "Email verification"
+    message = f'Click this link to verify your account http://localhost:8000/user/verify/{token}'
+    email_form = settings.EMAIL_HOST_USER
+    recipient_list = [email]
+    send_mail(subject, message, email_form, recipient_list )
