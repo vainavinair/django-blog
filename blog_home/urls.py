b677b0670ab10e4d3978dd56bfd088b0465f1c30
VanV <vainavinair2009558@gmail.com>
2023-05-02 17:04:30 +0530

    added update for post

--- blog_home/urls.py
+++ blog_home/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home, delete_post
+from .views import home, delete_post, update_post
  
 urlpatterns = [
     path('', home , name='blog-home'),     
     path('post/delete/<id>', delete_post , name='post-delete'),     
-    path('post/update/<id>', home , name='post-update'),     
+    path('post/update/<id>', update_post , name='post-update'),     
 ]
--- blog_home/views.py
+++ blog_home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import HttpResponseForbidden
 
 from django.contrib.auth.models import User
 
@@ -28,11 +29,30 @@ def home(request):
 
 
 def delete_post(request,id):
-    post = Posts.objects.filter(id=id).first()
+    # post = Posts.objects.filter(id=id).first()
+    post = get_object_or_404(Posts, id=id)
     if request.user == post.author:
         post.delete()
         return redirect('blog-home')
     else: 
         messages.info(request,f"You dont have the persmission to delete this post")
         return redirect('blog-home')
+    
+def update_post(request, id):
+    post = get_object_or_404(Posts, id=id)
+    if request.user == post.author:
+        if request.method == "POST":
+            title = request.POST.get('post-title')
+            content = request.POST.get('post-content')
+            post.title = title
+            post.content = content
+            post.save()
+            return redirect('blog-home')
+        else:
+            context = {'post': post}
+            return render(request, 'blog_home/update.html', context)
+    else: 
+        messages.info(request,f"You dont have the persmission to update this post")
+        return redirect('blog-home')
+
         
